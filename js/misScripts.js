513e920c766b8e8c6feed988b944151790fba34f
David Salinas FrontEnd Dev/ <dnarino@gmail.com>
2017-09-13 14:50:08 -0500

    animaciones:smooth-scroll

--- js/misScripts.js
+++ js/misScripts.js
@@ -1,2 +1,13 @@
 /* Iniciamos WOW*/
 new WOW().init();
+
+// Iniciamos SmoothScroll
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+    // Speed & Easing
+	speed: 500, // Integer. How fast to complete the scroll in milliseconds
+	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+	easing: 'easeOutQuad', // Easing pattern to use
+	customEasing: function (time) {}, // Function. Custom easing pattern
+});
+
