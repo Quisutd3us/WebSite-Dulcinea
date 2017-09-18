8d0405fa0e6bd447cb74794e2bc8d058e3c1756f
David Salinas FrontEnd Dev/ <dnarino@gmail.com>
2017-09-18 12:15:13 -0500

    css3:boton ir-arriba

--- js/misScripts.js
+++ js/misScripts.js
@@ -3,11 +3,18 @@ new WOW().init();
 
 // Iniciamos SmoothScroll
 
-var scroll = new SmoothScroll('a[href*="#"]',{
-    // Speed & Easing
-	speed: 500, // Integer. How fast to complete the scroll in milliseconds
+var scroll = new SmoothScroll('a[href*="#"]', {
+	// Speed & Easing
+	speed: 1500, // Integer. How fast to complete the scroll in milliseconds
 	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
 	easing: 'easeOutQuad', // Easing pattern to use
 	customEasing: function (time) {}, // Function. Custom easing pattern
 });
 
+// codigo boton ir arriba para q aparezca solo despues del header
+
+$(function () {
+	// alert('jquery ok');
+	// $
+	
+})
